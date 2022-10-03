bfd64f87a6cd0e7be966e429f0dcef6cc1a973c2
Philip Amoafo Boamah <pgod34@gmail.com>
2022-10-03 19:54:44 +0000

    added color, validation background color change etc

--- starter/script.js
+++ starter/script.js
@@ -35,8 +35,6 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too high!';
@@ -44,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '😭You lost the game';
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+      document.querySelector('number').style.width = '30rem';
       document.querySelector('.score').textContent = 0;
     }
 
@@ -55,6 +55,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '😭You lost the game';
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+      document.querySelector('number').style.width = '30rem';
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -72,3 +74,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('number').style.width = '15rem';
 });
+
+var numb;
